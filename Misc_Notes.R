650d51b7fe7e065dceb4ab5ec82777705a0d3028
adamreinschmidt16 <adam.reinschmidt16@ncf.edu>
2018-01-17 16:29:11 -0500

    change files

---
+++ Misc_Notes.R
@@ -0,0 +1,7 @@
+# How to add text under each icon (and eventually change the icon)
+ # Edit: How do I alter the .html file correctly to add the text?
+ # Edit: Figured it out: in the list tag of skills.html, add a <p>{{ .description}}</p> underneath the <h3>{{ .title}}</h3>
+# Do I need to copy over the index.html from themes/hugo-highlight-theme/layouts/partials to the upper-level layouts/partials if I want to override?
+# Resizing/positioning images in Hugo + how to properly implement Rmd code (as I already know some orientating functions for knitr)
+# Adding more pages (is it as simple as creating new partials and then new content?)
+# What the heck does the overlay.png do?
